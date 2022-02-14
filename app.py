6cdf6bccb92afd67e1c8d22a44cba07956ff27bf
Matvey <tem12qaz@yandex.ru>
2022-02-14 19:48:24 +0300

    text extr

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def upload():
                 with open('table.csv', 'r', encoding='utf-8') as f:
                     for row in f:
                         row = row.split(';')
-                        invoice = Invoice.query.filter(Invoice.number == row[0]).fetch()
+                        invoice = Invoice.query.filter_by(number=row[0]).all()
                         if invoice:
                             invoice.email += f' {row[1]}'
                             invoice.place += f' {row[2]}'
