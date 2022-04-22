278c548e0c4e7e481f2fe7d82e16d198d1e11d4b
tem12qaz <tem12qaz@yandex.ru>
2022-04-22 18:19:29 +0300

    add vko

--- parser.py
+++ parser.py
@@ -52,11 +52,11 @@ def dme_get_status(status_text: str, departure_time: str):
 
 
 def vko_get_status(status_text: str, _):
-    if status_text == 'Принята на склад':
+    if 'Принята на склад' in status_text:
         return 1
-    elif status_text == 'Груз в зоне комплектации':
+    elif 'Груз в зоне комплектации in status_text:' in status_text:
         return 2
-    elif status_text == 'Улетела':
+    elif 'Улетела' in status_text:
         return 3
     else:
         return 0
